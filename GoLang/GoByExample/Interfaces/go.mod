4a06b6020b9e5e0349d97cf7713c4f7f06ace6a4
Ron Kitay <ronk@qwilt.com>
2023-11-17 12:09:01 +0200

    Go by Example - Add Interfaces as a Module

---
+++ GoLang/GoByExample/Interfaces/go.mod
@@ -0,0 +1,3 @@
+module ronkitay.com/GoByExample/Interfaces.v0
+
+go 1.21.1
--- GoLang/GoByExample/Interfaces/interfaces.go
+++ GoLang/GoByExample/Interfaces/interfaces.go
@@ -1,11 +1,48 @@
 package main
 
-import {
+import (
 	"fmt"
 	"math"
+)
+
+type geometry interface {
+	area() float64
+	perimeter() float64
+}
+
+type rectangle struct {
+	height, width float64
+}
+
+type circle struct {
+	radios float64
+}
+
+func (r rectangle) area() float64 {
+	return r.height * r.width
+}
+
+func (r rectangle) perimeter() float64 {
+	return (r.height + r.width) * 2
+}
+
+func (c circle) area() float64 {
+	return c.radios * c.radios * math.Pi
+}
+
+func (c circle) perimeter() float64 {
+	return 2 * c.radios * math.Pi
+}
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perimeter())
 }
 
 func main() {
 
+	measure(rectangle{1, 1})
+	measure(circle{12.5})
 
 }
