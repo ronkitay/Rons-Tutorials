6acba1c53d2e6352a18d8fb7669183934046c035
Ron Kitay <ronk@qwilt.com>
2023-12-08 18:35:44 +0200

    make new-module.sh easier to use

--- GoLang/GoByExample/new-module.sh
+++ GoLang/GoByExample/new-module.sh
@@ -1,30 +1,35 @@
 #!/bin/sh
 
-if [ $# -ne 2 ]; then
-  echo "Expecting exactly one argument"
+if [ $# -lt 1 ]; then
+  echo "Expecting at least one argument"
   exit 1
 fi
 
+MODULE_HUMAN_NAME=$(echo $* | tr -d ' ')
+MODULE_CODE_NAME=$(echo $* | tr ' ' '-' | tr 'A-Z' 'a-z')
+
 let next=$(fd -t d  | sort -n | tail -n 1 | cut -d '.' -f1)+1;
 
-mkdir "${next}.$1"
+MODULE_DIR_NAME="${next}.${MODULE_HUMAN_NAME}"
 
-cd "${next}.$1"
+mkdir "${MODULE_DIR_NAME}"
+
+cd "${MODULE_DIR_NAME}"
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-sed -i .bak "$ s|)|	./GoLang/GoByExample/${next}.$1|" ${REPO_ROOT}/go.work
+sed -i .bak "$ s|)|	./GoLang/GoByExample/${MODULE_DIR_NAME}|" ${REPO_ROOT}/go.work
 
 printf "\n)" >> ${REPO_ROOT}/go.work
 
 rm ${REPO_ROOT}/go.work.bak
 
-echo "module ronkitay.com/GoByExample/${1}.v0" > go.mod; 
+echo "module ronkitay.com/GoByExample/${MODULE_DIR_NAME}.v0" > go.mod; 
 echo "" >> go.mod; 
 printf "go 1.21.1" >> go.mod
 
-echo "package main" > ${2}.go
-echo "" >> ${2}.go
-echo "func main() {" >> ${2}.go
-echo "" >> ${2}.go
-echo "}" >> ${2}.go
+echo "package main" > ${MODULE_CODE_NAME}.go
+echo "" >> ${MODULE_CODE_NAME}.go
+echo "func main() {" >> ${MODULE_CODE_NAME}.go
+echo "" >> ${MODULE_CODE_NAME}.go
+echo "}" >> ${MODULE_CODE_NAME}.go
