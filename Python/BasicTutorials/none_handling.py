4ecc4f9759303276305a4ba9afb26bb3a6dc9702
Ron Kitay <ronkitay@gmail.com>
2025-10-26 09:59:24 +0200

    stuff

---
+++ Python/BasicTutorials/none_handling.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from rons_tutorial_formatting import *
+
+print_block_separator()
+
+none_list = []
+
+for nothing in none_list:
+    print(nothing)
+
+end_block(True)
+
